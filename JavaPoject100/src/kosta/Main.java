7168267a3994762109072f54dc8ea0d5f90aa59b
dolsam75 <dolsam77@naver.com>
2019-05-22 12:41:09 +0900

    66

--- JavaPoject100/src/kosta/Main.java
+++ JavaPoject100/src/kosta/Main.java
@@ -8,10 +8,15 @@ public class Main {
 		
 
 		//name print add park 1111
+<<<<<<< master
 		for(int i=0;i<names.length;i++){
 			System.out.print(names[i] +  ", ");
 			System.out.println("Ãß°¡");		
 
+=======
+		for(String name : names){
+			System.out.print(name + ", ");
+>>>>>>> 3d3d1d8 4
 		}
 	}
 
