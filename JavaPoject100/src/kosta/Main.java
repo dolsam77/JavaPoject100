5e1088cfbfa30267d6b321e8ae8ee169ccf2497d
dolsam77 <domsam77@nate.com>
2019-05-22 12:05:08 +0900

    first commit

---
+++ JavaPoject100/src/kosta/Main.java
@@ -0,0 +1,16 @@
+package kosta;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String names[] = {"kim", "lee", "seo"};
+		
+		//name print
+		for(int i=0;i<names.length;i++){
+			System.out.print(names[i] +  ", ");
+		}
+
+	}
+
+}
