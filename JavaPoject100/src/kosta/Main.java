2da31bbe655aa476c488c028254b75aa8fd8f817
dolsam75 <dolsam77@naver.com>
2019-05-22 12:39:31 +0900

    55

--- JavaPoject100/src/kosta/Main.java
+++ JavaPoject100/src/kosta/Main.java
@@ -6,10 +6,12 @@ public class Main {
 		
 		String names[] = {"kim", "lee", "seo", "park"};
 		
+
 		//name print add park 1111
-		for(String name : names){
-			System.out.print(name + ", ");
-			System.out.println("aa");
+		for(int i=0;i<names.length;i++){
+			System.out.print(names[i] +  ", ");
+			System.out.println("Ãß°¡");		
+
 		}
 	}
 
